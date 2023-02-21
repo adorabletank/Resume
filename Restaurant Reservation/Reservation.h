1068b63825fb3efa4f5db38990e04fc3dd1a064f
adorabletank <95323337+adorabletank@users.noreply.github.com>
2023-02-21 17:12:12 -0500

    Create Reservation.h

---
+++ Restaurant Reservation/Reservation.h
@@ -0,0 +1,30 @@
+
+#ifndef SDDS_RESERVATION_H
+#define SDDS_RESERVATION_H
+
+#include <iostream>
+#include <string>
+
+constexpr int ID_SIZE{ 11 }; // Size of 11 because reservation IDs are meant to be
+                           // printed into a field of size 10.
+
+namespace sdds {
+
+    class Reservation
+    {
+        char m_id[ID_SIZE];
+        std::string m_name;
+        std::string m_email;
+        unsigned m_guestCount;
+        unsigned char m_day;  // Number between 1-28
+        unsigned char m_hour; // Number between 1-24
+    public:
+        Reservation();
+        Reservation(const std::string& res);
+        void update(int day, int time);
+        friend std::ostream& operator<<(std::ostream&, const Reservation&);
+    };
+
+} // namespace sdds
+
+#endif
